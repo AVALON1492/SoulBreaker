5b957ac07e5fc7054ae973efe7a94f82802f3689
Alex Lozano Martín <alexlozanomartin1492@gmail.com>
2023-08-04 00:01:54 +0200

    Cambios de rutas de archivos

--- js/version.js
+++ js/version.js
@@ -1,15 +1,80 @@
-const root = document.querySelector(":root");
+let showVersion = false;
+class VERSION {
+    constructor(release, features = false, issues = false, textFeatures = null, textIssues = null) {
+        this.release = release;
+        this.features = features;
+        this.issues = issues;
+        this.textFeatures = textFeatures;
+        this.textIssues = textIssues;
+        this.faxVersion = document.createElement("div");
+        this.listNotes = document.createElement("ul");
+    }
 
-if (localStorage["valueTheme"] == "false") {
-    root.style.setProperty("--bgText", "rgba(0, 0, 0, 0.8)");
-    root.style.setProperty("--bgTextSoft", "rgba(0, 0, 0, 0.5)");
-} else if (localStorage["valueTheme"] == "true") {
-    root.style.setProperty("--bgText", "rgba(255, 255, 255, 1)");
-    root.style.setProperty("--bgTextSoft", "rgba(255, 255, 255, 0.5)");
+    show() {
+        this.faxVersion.className = "boxFaxVersion";
+        this.faxVersion.innerText = this.release;
+        listVersions.appendChild(this.faxVersion);
+
+        this.listNotes.className = "boxList";
+        listVersions.appendChild(this.listNotes);
+
+        if (this.issues) {
+            const idIssue = document.createElement("div");
+            idIssue.className = "boxMisc"
+            idIssue.innerText = "Errores corregidos"
+            this.listNotes.appendChild(idIssue);
+        }
+
+        if (this.features) {
+            const idFeature = document.createElement("div");
+            idFeature.className = "boxMisc"
+            idFeature.innerText = "Mejoras"
+            this.listNotes.appendChild(idFeature);
+        }
+
+        for(let i = 0; i < this.textFeatures.length; i++) {
+            const list = document.createElement("li");
+            list.innerHTML = this.textFeatures[i];
+            this.listNotes.appendChild(list);
+        }
+
+        for(let i = 0; i < this.textIssues.length; i++) {
+            const list = document.createElement("li");
+            list.innerHTML = this.textIssues[i];
+            this.listNotes.appendChild(list);
+        }
+    }
+
+    hide() {
+        listVersions.removeChild(this.faxVersion);
+        this.listNotes.innerText = "";
+        listVersions.removeChild(this.listNotes);
+    }
 }
 
-const values = JSON.parse(localStorage["valueColor"]);
+const alpha2 = new VERSION("Alpha 2", false, true,
+[],
+["Se ha corregido de que los iconos de entrada no cargaban correctamente.",
+"Se ha corregido un grave error que no cargaban correctamente en la sección de versiones (A).",
+"Gracias a ese error corregido (A), se ha mejorado notablemente el rendimiento al accionar las entradas de secciones.",
+"Otros pequeños errores corregidos."]
+);
 
-root.style.setProperty("--colorMain", "hsl(" + values[0] + ", " + values[1] + "%, " + values[2] + "%)");
-root.style.setProperty("--colorMainOpac", "hsl(" + values[0] + ", " + values[1] + "%, " + values[2] + "%, 0.15)");
-root.style.setProperty("--colorMainFill", "hsl(" + values[0] + ", " + values[1] + "%, " + values[2] + "%, 0.3)");
+const alpha1 = new VERSION("Alpha 1", false, false,
+["Prelanzamiento del juego en público."],
+[]
+);
+
+goVersions.addEventListener("click", () => {
+    showVersion = true;
+    alpha2.show();
+    alpha1.show();
+});
+
+goMain.addEventListener("click", () => {
+    if (showVersion) {
+        showVersion = false;
+        alpha2.hide();
+        alpha1.hide();
+    }
+})
